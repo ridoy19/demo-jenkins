1c3b3a846aea9697dda8aaf6cf60b266edbe54b5
jakaria_ridoy <jh_ridoy@yahoo.com>
2022-04-07 15:44:12 +0600

    init

---
+++ src/main/java/com/jakaria/demojenkins/DemoJenkinsApplication.java
@@ -0,0 +1,22 @@
+package com.jakaria.demojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsApplication.class, args);
+	}
+
+	@GetMapping
+	public ResponseEntity<String> sayHello() {
+		return new ResponseEntity<>("Hello, from Jenkins", HttpStatus.OK);
+	}
+}
---
+++ src/test/java/com/jakaria/demojenkins/DemoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakaria.demojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
